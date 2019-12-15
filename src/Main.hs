a49a4f4966b356e7d099385b288ed376980db225
d86leader <d86leader@mail.com>
2019-12-15 20:18:45 +0700

    read -> readMaybe; some style changes

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,25 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE LambdaCase #-}
 module Main where
 
-import Database.Control          (makeConnectionPool, doMigrations, runDatabase, SqlM)
-import Database.Types            (Rights)
-import Data.Monoid               ((<>))
-import Control.Monad.Fail        (fail)
-import Control.Monad.IO.Class    (liftIO)
-import Data.Text                 (Text, words, unwords, unpack)
-import qualified Data.Text.IO as TIO
 import Actions
-import Database.Persist          (entityVal, selectFirst, count, (==.))
-import Database.Types            (User, Object, EntityField ( UserName, UserPassword, ObjectName ))
-import System.IO                 (hFlush, stdout)
-import Prelude hiding            (read, take, fail, words, unwords)
-import qualified Prelude
+import Control.Monad.Fail     (fail)
+import Control.Monad.IO.Class (liftIO)
+import Data.Monoid            ((<>))
+import Data.Text              (Text, unpack, unwords, words)
+import Database.Control
+    (SqlM, doMigrations, makeConnectionPool, runDatabase)
+import Database.Persist       (count, entityVal, selectFirst, (==.))
+import Database.Persist.Sql   (transactionSave)
+import Database.Types         (Rights)
+import Database.Types
+    (EntityField (ObjectName, UserName, UserPassword), Object, User)
+import System.IO              (hFlush, stdout)
+import Text.Read              (readMaybe)
 
-import Database.Persist.Sql (transactionSave)
+import qualified Data.Text.IO as TIO
+
+import Prelude hiding (fail, read, take, unwords, words)
 
 checkShould :: SqlM (Either Text a) -> SqlM a
 checkShould v = v >>= \case
@@ -30,73 +33,91 @@ parseStr current_user str =
         "create" : "user" : user : password : _ -> do
             _ <- checkShould $ createUser user password
             pure "The user is created"
-        
+
         "create" : "object" : name : rest -> do
-            let text = unwords rest 
-            _ <- checkShould $ createObject name text 
+            let text = unwords rest
+            _ <- checkShould $ createObject name text
             pure "The object is created"
-        
+
         "write" : object : rest -> do
             obj <- checkShould $ getObject object
             let text = unwords rest
             _ <- checkShould $ write current_user obj text
             pure "Object is updated"
-        
+
         "read" : object : _ -> do
-            obj <- checkShould $ getObject object 
-            checkShould $ read current_user obj 
-        
-        "grant" : right : "for" : "object" : object : "to" : user : _ -> do
-            let r = Prelude.read (unpack right) :: Rights -- "Prelude.read: no parse" if not Read, Write, ect.
             obj <- checkShould $ getObject object
-            usr <- checkShould $ getUser user
-            _ <- checkShould $ grant (r, obj)  current_user usr
-            pure "The right is granted"
-        
-        "grant" : right : "for" : "user" : object : "to" : user : _ -> do
-            let r = Prelude.read (unpack right) :: Rights -- "Prelude.read: no parse" if not Read, Write, ect.
-            obj <- checkShould $ getUser object
-            usr <- checkShould $ getUser user
-            _ <- checkShould $ grant (r, obj)  current_user usr
-            pure "The right is granted"
+            checkShould $ read current_user obj
 
-        "take" : right : "to" : "object" : object : "from" : user : _ -> do 
-            let r = Prelude.read (unpack right) :: Rights -- "Prelude.read: no parse" if not Read, Write, ect.
-            obj <- checkShould $ getObject object
-            usr <- checkShould $ getUser user
-            _ <- checkShould $ take (r, obj)  current_user usr
-            pure "The right is taken"
+        "grant" : right : "for" : "object" : object : "to" : user : _ ->
+            case readMaybe (unpack right) :: Maybe Rights
+            of
+                Nothing -> pure "can't parse rights"
+                Just r -> do
+                    obj <- checkShould $ getObject object
+                    usr <- checkShould $ getUser user
+                    _ <- checkShould $ grant (r, obj)  current_user usr
+                    pure "The right is granted"
 
-        "take" : right : "to" : "user" : object : "from" : user : _ -> do 
-            let r = Prelude.read (unpack right) :: Rights -- "Prelude.read: no parse" if not Read, Write, ect.
-            obj <- checkShould $ getUser object
-            usr <- checkShould $ getUser user
-            _ <- checkShould $ take (r, obj)  current_user usr
-            pure "The right is taken"
+        "grant" : right : "for" : "user" : object : "to" : user : _ ->
+            case readMaybe (unpack right) :: Maybe Rights
+            of
+                Nothing -> pure "can't parse rights"
+                Just r -> do
+                    obj <- checkShould $ getUser object
+                    usr <- checkShould $ getUser user
+                    _ <- checkShould $ grant (r, obj)  current_user usr
+                    pure "The right is granted"
 
-        "dispose" : right : "to" : "object" : object : _ -> do
-            let r = Prelude.read (unpack right) :: Rights -- "Prelude.read: no parse" if not Read, Write, ect.
-            obj <- checkShould $ getObject object
-            _ <- checkShould $ dispose r current_user obj
-            pure "The right is disposed"
-        
-        "dispose" : right : "to" : "user" : object : _ -> do
-            let r = Prelude.read (unpack right) :: Rights -- "Prelude.read: no parse" if not Read, Write, ect.
-            obj <- checkShould $ getUser object
-            _ <- checkShould $ dispose r current_user obj
-            pure "The right is disposed"
-       
+        "take" : right : "to" : "object" : object : "from" : user : _ ->
+            case readMaybe (unpack right) :: Maybe Rights
+            of
+                Nothing -> pure "can't parse rights"
+                Just r -> do
+                    obj <- checkShould $ getObject object
+                    usr <- checkShould $ getUser user
+                    _ <- checkShould $ take (r, obj)  current_user usr
+                    pure "The right is taken"
+
+        "take" : right : "to" : "user" : object : "from" : user : _ ->
+            case readMaybe (unpack right) :: Maybe Rights
+            of
+                Nothing -> pure "can't parse rights"
+                Just r -> do
+                    obj <- checkShould $ getUser object
+                    usr <- checkShould $ getUser user
+                    _ <- checkShould $ take (r, obj)  current_user usr
+                    pure "The right is taken"
+
+        "dispose" : right : "to" : "object" : object : _ ->
+            case readMaybe (unpack right) :: Maybe Rights
+            of
+                Nothing -> pure "can't parse rights"
+                Just r -> do
+                    obj <- checkShould $ getObject object
+                    _ <- checkShould $ dispose r current_user obj
+                    pure "The right is disposed"
+
+        "dispose" : right : "to" : "user" : object : _ ->
+            case readMaybe (unpack right) :: Maybe Rights
+            of
+                Nothing -> pure "can't parse rights"
+                Just r -> do
+                    obj <- checkShould $ getUser object
+                    _ <- checkShould $ dispose r current_user obj
+                    pure "The right is disposed"
+
         "exit" : [] -> pure ""
-        
+
         _ -> pure "Incorrect input"
-    
+
 
 communicate :: User -> SqlM ()
 communicate user = do
     liftIO . putStr $ "> "
     liftIO . hFlush $ stdout
     str <- liftIO TIO.getLine
-    response <- parseStr user str 
+    response <- parseStr user str
     liftIO . TIO.putStrLn $ response
     _ <- transactionSave
     if response == ""
@@ -125,7 +146,7 @@ getUser name = do
     mbUser <- selectFirst [UserName ==. name] []
     case mbUser of
         Nothing   -> return . Left $ "User with this name doesn't exist"
-        Just user -> return . Right . entityVal $ user 
+        Just user -> return . Right . entityVal $ user
 
 
 getObject :: Text -> SqlM (Either Text Object)
@@ -133,8 +154,8 @@ getObject name = do
     mbObject <- selectFirst [ObjectName ==. name] []
     case mbObject of
         Nothing     -> return . Left $ "Object with this name doesn't exist"
-        Just object -> return . Right . entityVal $ object 
-        
+        Just object -> return . Right . entityVal $ object
+
 
 authorize :: Text -> Text -> SqlM (Either Text User)
 authorize name pwd = do
@@ -151,6 +172,7 @@ main = do
     pool <- makeConnectionPool
     runDatabase pool $ do
         doMigrations
+        _ <- checkShould $ createUser "admin" "123"
         name <- liftIO getUserName
         pwd  <- liftIO getPassword
         user <- checkShould $ authorize name pwd
@@ -158,7 +180,6 @@ main = do
 
         -- liftIO . putStrLn $ "hello database"
         --
-        -- hitler <- checkShould $ createUser "hitler" "hitlerpass"
         -- book   <- checkShould $ createObject "kampf" "mein"
         -- add all rights to this book
         -- forM_ [Read, Write, Take, Grant] $ \right -> do
